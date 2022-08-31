2e1495ebf0955e808b62a2915a79a66ce338dbdf
vidm27 <davidmoran464@gmail.com>
2022-08-31 00:41:11 -0500

    colores del tema por default

--- src/App.vue
+++ src/App.vue
@@ -14,8 +14,20 @@ export default {
 </script>
 
 <style>
+  @import url('https://fonts.googleapis.com/css2?family=League+Spartan:wght@300;400;700&display=swap');
+:root{
+  --bg-default-color: #3A4663;
+  --bg-promt-default-color: #181F33;
+  --bg-button-default-color: #EAE3DC;
+  --bg-button-equal-default-color: #D03F2F;
+  --bg-button-delete-default-color: #647198;
+  --color-default-text-keyboar: #434A59;
+  --color-default-text-alternative: #ffffff;
+}
 body{
   padding: 0;
   margin: 0;
+  background: var(--bg-default-color);
+  font-family: 'League Spartan', sans-serif;
 }
 </style>
